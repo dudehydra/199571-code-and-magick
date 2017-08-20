4db62268c1e2ee4036888754fa94daf9066e8f9e
Peresmekhin Aleksey <aleksfers@icloud.com>
2017-08-20 21:12:13 +0500

    Правка замечаний/Добавление функции получения неупорядоченого массива из упорядоченого

--- js/setup.js
+++ js/setup.js
@@ -3,47 +3,61 @@ var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_INIT = {
+  getRandomWizards: function (names, lastNames, coatColors, eyesColors) {
+    var maxWizards = 4;
+    var wizardsArr = [];
+    var namesNumberArr = getRandomArr(names.length);
+    var lastNamesNumberArr = getRandomArr(lastNames.length);
+    for (var i = 0; i < maxWizards; i++) {
+      wizardsArr[i] = {
+        name: names[namesNumberArr[i]] + ' ' + lastNames[lastNamesNumberArr[i]],
+        eyesColor: getRandomElement(eyesColors),
+        coatColor: getRandomElement(coatColors)
+      };
+    }
+    return wizardsArr;
+  },
+  drawWizard: function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    return wizardElement;
+  },
+  insertWizardFragments: function (wizard, fragment, elem) {
+    for (var i = 0; i < wizard.length; i++) {
+      fragment.appendChild(this.drawWizard(wizard[i]));
+    }
+    elem.appendChild(fragment);
+  },
+  removeClassHidden: function (elem) {
+    elem.classList.remove('hidden');
+  },
+  init: function (wizard, fragment, elem, dialogElem, setupElem) {
+    this.removeClassHidden(dialogElem);
+    this.insertWizardFragments(wizard, fragment, elem);
+    this.removeClassHidden(setupElem);
+  }
+};
 var userDialog = document.querySelector('.setup');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 var listElement = userDialog.querySelector('.setup-similar-list');
 var setupSimilar = userDialog.querySelector('.setup-similar');
 var setupListFragment = document.createDocumentFragment();
 var wizards;
-var getRandomNumber = function (length) {
-  return Math.round(Math.random() * (length - 1));
+var getRandomElement = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
-var getRandomName = function (names, lastNames) {
-  return names[getRandomNumber(names.length)] + ' ' + lastNames[getRandomNumber(lastNames.length)];
+var compareRandom = function () {
+  return Math.random() - 0.5;
 };
-var getRandomWizardOptions = function (names, lastNames, coatColor, eyesColor) {
-  var maxCount = 4;
-  var options = [];
-  for (var i = 0; i < maxCount; i++) {
-    options[i] = {
-      name: getRandomName(names, lastNames),
-      coatColor: coatColor[getRandomNumber(coatColor.length)],
-      eyesColor: eyesColor[getRandomNumber(eyesColor.length)]
-    };
+var getRandomArr = function (arrayLength) {
+  var numbersArray = [];
+  for (var j = 0; j < arrayLength; j++) {
+    numbersArray[j] = j;
   }
-  return options;
+  return numbersArray.sort(compareRandom);
 };
-var drawWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-  return wizardElement;
-};
-var insertWizardFragments = function (wizard, fragment, elem) {
-  for (var i = 0; i < wizard.length; i++) {
-    fragment.appendChild(drawWizard(wizard[i]));
-  }
-  elem.appendChild(fragment);
-};
-var removeClassHidden = function (elem) {
-  elem.classList.remove('hidden');
-};
-wizards = getRandomWizardOptions(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
-removeClassHidden(userDialog);
-insertWizardFragments(wizards, setupListFragment, listElement);
-removeClassHidden(setupSimilar);
+wizards = WIZARDS_INIT.getRandomWizards(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
+WIZARDS_INIT.init(wizards, setupListFragment, listElement, userDialog, setupSimilar);
