cd451ffe8caab6870b334c98c6b5f69160c97785
Peresmekhin Aleksey <aleksfers@icloud.com>
2017-08-24 09:32:49 +0500

    Определние функций для обработчиков событий

--- js/setup.js
+++ js/setup.js
@@ -35,12 +35,40 @@ var WIZARDS_INIT = {
     elem.classList.remove('hidden');
   },
   init: function (wizard, fragment, elem, dialogElem, setupElem) {
-    this.showNode(dialogElem);
+    // this.showNode(dialogElem);
     this.insertWizardFragments(wizard, fragment, elem);
     this.showNode(setupElem);
   }
 };
+var KEY_CODE = {
+  enter: 13,
+  esc: 27
+};
+var SETUP_HANDLERS = {
+  openPopup: function () {
+    userDialog.classList.remove('hidden');
+    document.addEventListener('keydown', this.popupEscPressHandler);
+  },
+  closePopup: function () {
+    userDialog.classList.add('hidden');
+    document.removeEventListener('keydown', this.popupEscPressHandler);
+  },
+  popupEscPressHandler: function (evt) {
+    if (evt.keyCode === KEY_CODE.esc) {
+      SETUP_HANDLERS.closePopup();
+    }
+  },
+  popupEnterPressHandler: function (evt) {
+    if (evt.keyCode === KEY_CODE.enter && this === setupOpen) {
+      SETUP_HANDLERS.openPopup();
+    } else if (evt.keyCode === KEY_CODE.enter && this === setupClose) {
+      SETUP_HANDLERS.closePopup();
+    }
+  }
+};
 var userDialog = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 var listElement = userDialog.querySelector('.setup-similar-list');
 var setupSimilar = userDialog.querySelector('.setup-similar');
@@ -56,5 +84,15 @@ var randomizeOrder = function (array) {
   var arrayClone = array.slice(0, array.length);
   return arrayClone.sort(compareRandom);
 };
-wizards = WIZARDS_INIT.getRandomWizards(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
+wizards = WIZARDS_INIT.generateSimilarWizards(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
 WIZARDS_INIT.init(wizards, setupListFragment, listElement, userDialog, setupSimilar);
+setupOpen.addEventListener('click', function () {
+  SETUP_HANDLERS.openPopup();
+});
+setupClose.addEventListener('click', function () {
+  SETUP_HANDLERS.closePopup();
+});
+setupOpen.addEventListener('keydown', SETUP_HANDLERS.popupEnterPressHandler);
+setupClose.addEventListener('keydown', SETUP_HANDLERS.popupEnterPressHandler);
+
+
