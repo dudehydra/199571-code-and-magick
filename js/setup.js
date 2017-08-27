e2f1b1f0104403dafe3157f6c581f2015465790c
Peresmekhin Aleksey <aleksfers@icloud.com>
2017-08-27 13:12:01 +0500

    Разделяй и Влавствуй

--- js/setup.js
+++ js/setup.js
@@ -1,133 +1,63 @@
 /* eslint-disable no-invalid-this */
 'use strict';
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-var WIZARDS_INIT = {
-  generateSimilarWizards: function (names, lastNames, coatColors, eyesColors) {
-    var wizardsCount = 4;
-    var wizardsList = [];
-    var namesShuffled = randomizeOrder(names);
-    var lastNamesShuffled = randomizeOrder(lastNames);
-    for (var i = 0; i < wizardsCount; i++) {
-      wizardsList[i] = {
-        name: namesShuffled[i] + ' ' + lastNamesShuffled[i],
-        eyesColor: getRandomElement(eyesColors),
-        coatColor: getRandomElement(coatColors)
-      };
+(function () {
+  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var WIZARDS_INIT = {
+    generateSimilarWizards: function (names, lastNames, coatColors, eyesColors) {
+      var wizardsCount = 4;
+      var wizardsList = [];
+      var namesShuffled = window.calculateFunctions.randomizeOrder(names);
+      var lastNamesShuffled = window.calculateFunctions.randomizeOrder(lastNames);
+      for (var i = 0; i < wizardsCount; i++) {
+        wizardsList[i] = {
+          name: namesShuffled[i] + ' ' + lastNamesShuffled[i],
+          eyesColor: window.calculateFunctions.getRandomElement(eyesColors),
+          coatColor: window.calculateFunctions.getRandomElement(coatColors)
+        };
+      }
+      return wizardsList;
+    },
+    drawWizard: function (wizard) {
+      var wizardElement = similarWizardTemplate.cloneNode(true);
+      wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+      wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+      wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+      return wizardElement;
+    },
+    insertWizardFragments: function (wizard, fragment, elem) {
+      for (var i = 0; i < wizard.length; i++) {
+        fragment.appendChild(this.drawWizard(wizard[i]));
+      }
+      elem.appendChild(fragment);
+    },
+    showNode: function (elem) {
+      elem.classList.remove('hidden');
+    },
+    init: function (wizard, fragment, elem, setupElem) {
+      this.insertWizardFragments(wizard, fragment, elem);
+      this.showNode(setupElem);
     }
-    return wizardsList;
-  },
-  drawWizard: function (wizard) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-    return wizardElement;
-  },
-  insertWizardFragments: function (wizard, fragment, elem) {
-    for (var i = 0; i < wizard.length; i++) {
-      fragment.appendChild(this.drawWizard(wizard[i]));
-    }
-    elem.appendChild(fragment);
-  },
-  showNode: function (elem) {
-    elem.classList.remove('hidden');
-  },
-  init: function (wizard, fragment, elem, setupElem) {
-    this.insertWizardFragments(wizard, fragment, elem);
-    this.showNode(setupElem);
-  }
-};
-var KEY_CODE = {
-  enter: 13,
-  esc: 27
-};
-var setupHandlers = {
-  openPopup: function () {
-    userDialog.classList.remove('hidden');
-    document.addEventListener('keydown', setupHandlers.popupEscPressHandler);
-  },
-  closePopup: function () {
-    userDialog.classList.add('hidden');
-    document.removeEventListener('keydown', setupHandlers.popupEscPressHandler);
-  },
-  popupEscPressHandler: function (evt) {
-    if (evt.keyCode === KEY_CODE.esc && document.activeElement !== setupUserName) {
-      setupHandlers.closePopup();
-    }
-  },
-  popupEnterPressHandler: function (evt) {
-    if (evt.keyCode === KEY_CODE.enter && this === setupOpen) {
-      setupHandlers.openPopup();
-    } else if (evt.keyCode === KEY_CODE.enter && this === setupClose) {
-      setupHandlers.closePopup();
-    }
-  },
-  wizardElemFill: function (elem, array, index) {
-    elem.style.fill = array[index];
-  },
-  wizardFireballFill: function (elem, array, index) {
-    elem.style.backgroundColor = array[index];
-  }
-};
-var userDialog = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = document.querySelector('.setup-close');
-var setupUserName = document.querySelector('.setup-user-name');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-var listElement = userDialog.querySelector('.setup-similar-list');
-var setupSimilar = userDialog.querySelector('.setup-similar');
-var setupListFragment = document.createDocumentFragment();
-var wizards;
-var setupWizard = document.querySelector('.wizard');
-var setupWizardCoat = document.querySelector('.wizard-coat');
-var setupWizardEyes = document.querySelector('.wizard-eyes');
-var setupFireball = document.querySelector('.setup-fireball-wrap');
-var colorsIndex = {
-  coat: 0,
-  eyes: 0,
-  fireball: 0
-};
-var getRandomElement = function (arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
-var compareRandom = function () {
-  return Math.random() - 0.5;
-};
-var randomizeOrder = function (array) {
-  var arrayClone = array.slice(0, array.length);
-  return arrayClone.sort(compareRandom);
-};
-var incrementColorIndex = function (index, array) {
-  var indexNumber = index;
-  indexNumber = (indexNumber + 1) % array.length;
-  return indexNumber;
-};
-wizards = WIZARDS_INIT.generateSimilarWizards(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
-WIZARDS_INIT.init(wizards, setupListFragment, listElement, setupSimilar);
-setupOpen.addEventListener('click', setupHandlers.openPopup);
-setupOpen.addEventListener('keydown', setupHandlers.popupEnterPressHandler);
-setupClose.addEventListener('click', setupHandlers.closePopup);
-setupClose.addEventListener('keydown', setupHandlers.popupEnterPressHandler);
-setupWizard.addEventListener('click', function (evt) {
-  var evtTarget = evt.target;
-  switch (evtTarget) {
-    case setupWizardCoat:
-      setupHandlers.wizardElemFill(setupWizardCoat, COAT_COLORS, colorsIndex.coat);
-      colorsIndex.coat = incrementColorIndex(colorsIndex.coat, COAT_COLORS);
-      break;
-    case setupWizardEyes:
-      setupHandlers.wizardElemFill(setupWizardEyes, EYES_COLORS, colorsIndex.eyes);
-      colorsIndex.eyes = incrementColorIndex(colorsIndex.eyes, EYES_COLORS);
-      break;
-  }
-  return false;
-});
-setupFireball.addEventListener('click', function () {
-  setupHandlers.wizardFireballFill(this, FIREBALL_COLORS, colorsIndex.fireball);
-  colorsIndex.fireball = incrementColorIndex(colorsIndex.fireball, FIREBALL_COLORS);
-});
+  };
+  var userDialog = document.querySelector('.setup');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+  var listElement = userDialog.querySelector('.setup-similar-list');
+  var setupSimilar = userDialog.querySelector('.setup-similar');
+  var setupListFragment = document.createDocumentFragment();
+  var wizards = null;
+  wizards = WIZARDS_INIT.generateSimilarWizards(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
+  WIZARDS_INIT.init(wizards, setupListFragment, listElement, setupSimilar);
+  window.wizardsSettings = {
+    coatColors: COAT_COLORS,
+    eyesColors: EYES_COLORS,
+    userDialog: userDialog
+  };
+})();
+
+// Обработчики событий для диалога
+
+// Переменные
+
 
